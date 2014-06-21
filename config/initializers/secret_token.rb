bff5622a625cf6b4c1140b7a1c4ded0550c5a97c
Manu Kodiyan <mkodiyan@altheahealth.com>
2014-06-21 08:04:25 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '21c0df99e1f93bbb9674487b6b72672d6f1ad91773e676bb4cf3e42872bfe5e934508c0db8d4a3561e7be4c0dda0fc41843340981d13ba32cb9356ad1cc3710c'
